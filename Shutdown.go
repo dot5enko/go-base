3594e5dc6fa38a3d3df05f27ac5bde21c15d448e
dot5enko <sergiydotsenko@gmail.com>
2021-06-13 16:42:06 +0300

    add execute with interval func

--- Shutdown.go
+++ Shutdown.go
@@ -1,9 +1,11 @@
 package gobase
 
 import (
+	"context"
 	"os"
 	"os/signal"
 	"syscall"
+	"time"
 )
 
 // not safe, can raise panics
@@ -13,4 +15,17 @@ func OnShutdown(callback func()) {
 
 	<-c
 	callback()
+}
+
+func ExecuteWithInterval(ctx context.Context, duration time.Duration, task func()) {
+	end := false
+
+	for !end {
+		select {
+		case <-time.After(duration):
+			task()
+		case <-ctx.Done():
+			end = true
+		}
+	}
 }
